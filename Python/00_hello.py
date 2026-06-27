9aa867eb047dd3baa5a93a49509aaf63ff85c022
jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2026-06-27 13:33:31 -0500

    Nose

---
+++ Python/00_hello.py
@@ -0,0 +1,11 @@
+""" Comentarios
+con salto de linea"""
+#comentario una fila
+print("hola mundo")
+
+#type consultar el tipo de dato
+print(type("Hola python")) #tipo de datos str
+print(type(5)) #tipo de dato entero
+print(type(8.1)) #tipo de dato float
+print(type(5+2j)) #tipo de dato complex
+print(type(True)) #tipo de dato boolean
--- Python/Prueba1.py
+++
@@ -1,7 +0,0 @@
-""" Comentarios
-con salto de linea"""
-#comentario una fila
-print("hola mundo")
-
-#type consultar el tipo de dato
-print(type("Hola python"))
