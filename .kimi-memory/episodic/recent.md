f3e3f2b6453fd5c2b9f58a183a57828325e201e2
Jeffercastil <jeffrycastil91@gmail.com~>
2026-06-28 13:42:04 -0500

    mm

---
+++ .kimi-memory/episodic/recent.md
@@ -0,0 +1,11 @@
+# Recent Episodes
+
+## Entry (2026-06-22T02:48:41.666Z)
+
+tags: general
+
+Task: funcionas?
+User request: funcionas?
+Tools used: none
+Had errors: no
+
---
+++ Python/01_Variables.py
@@ -0,0 +1,12 @@
+
+#Variables
+myVariable = "My string variable"
+print (myVariable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+print(myVariable,my_int_variable,my_bool_variable)
